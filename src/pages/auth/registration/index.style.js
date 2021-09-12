4e3ac240b0df72596098cc122753d425375ecfe0
Murodbek <murodbek@thinkland.uz>
2021-09-12 10:50:26 +0500

    asdfas

--- src/pages/auth/registration/index.style.js
+++ src/pages/auth/registration/index.style.js
@@ -1,14 +1,26 @@
 import styled from "styled-components"
 export const Wrapper = styled.div`
     width: 100%;
-    height: 80vh;
+    min-height: 80vh;
+    margin:50x 0;
     display:flex;
-    align-items: center;
+    align-items: flex-start;
     justify-content:center;
+
+    .sc-cxFLGX.dhdUK{
+        margin:50px 0;
+    }
 `
 export const Container = styled.div`
-    width:400px;
+    /* width:400px; */
     height: auto;
     padding:10px;
-    background-color:green
+    box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.12);
+
+    .MuiPaper-elevation4{
+        box-shadow:none !important;
+    }
+    .MuiAppBar-colorDefault{
+        background-color:#fff;
+    }
 `
--- src/pages/auth/registration/reg.component.js
+++ src/pages/auth/registration/reg.component.js
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import TextInput from "../../../components/input"
+import {Container} from "../../../styles/container/index.style"
+import Button from "../../../components/button"
 const Index = () => {
     return (
         <div>
-            Registratsiya
+            <Container>
+                <TextInput title="E-mail kiriting" width="100%"/>
+            </Container>
+            <Container>
+                <TextInput title="Password kiriting" width="100%"/>
+            </Container>
+            <Container>
+                <TextInput title="E-mail kiriting" width="100%"/>
+            </Container>
+            <Container>
+                <TextInput title="Password kiriting" width="100%"/>
+            </Container>
+            <Container>
+                <TextInput title="Password kiriting" width="100%"/>
+            </Container>
+            <Container>
+                <Button>Kirish</Button>
+            </Container>
         </div>
     )
 }
