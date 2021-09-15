353782da7536b59cb2cb285323448484e387df43
Murodbek <murodbek@thinkland.uz>
2021-09-15 13:56:57 +0500

    adsafsf

---
+++ src/pages/fill.application/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Index = () => {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis hic veritatis nulla minus quasi eos odio quisquam atque recusandae quia minima sunt commodi repellat, quos error magnam unde consectetur provident.
+        </div>
+    )
+}
+
+export default Index
