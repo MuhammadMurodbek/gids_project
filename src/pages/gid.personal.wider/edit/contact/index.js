4f8097fdf5fede1831b8738b15c965acc82d41e1
Murodbek <murodbek@thinkland.uz>
2021-09-27 13:19:38 +0500

    sadfS

--- src/pages/gid.personal.wider/edit/contact/index.js
+++ src/pages/gid.personal.wider/edit/contact/index.js
@@ -1,9 +1,25 @@
+import { Grid } from '@material-ui/core'
 import React from 'react'
 import {Wrapper} from './style'
+import InputLabel from "../../../../components/molecules/input.labeled"
+import Button from "../../../../components/atom/button"
+import { Container } from '../../../../styles/container/index.style'
 const Index = () => {
     return (
         <Wrapper>
-            biopopop
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm={6} md={4}><InputLabel width="100%" label="Ism" placeholder="kiriting..."/> </Grid>
+                <Grid item xs={12} sm={6} md={4}><InputLabel width="100%" label="Ism" placeholder="kiriting..."/> </Grid>
+                <Grid item xs={12} sm={6} md={4}><InputLabel width="100%" label="Ism" placeholder="kiriting..."/> </Grid>
+                <Grid item xs={12} sm={6} md={4}><InputLabel width="100%" label="Ism" placeholder="kiriting..."/> </Grid>
+                <Grid item xs={12} sm={6} md={4}><InputLabel width="100%" label="Ism" placeholder="kiriting..."/> </Grid>
+                <Grid item xs={12} sm={6} md={4}><InputLabel width="100%" label="Ism" placeholder="kiriting..."/> </Grid>
+                <Grid item xs={12} sm={6} md={4}><InputLabel width="100%" label="Ism" placeholder="kiriting..."/> </Grid>
+                <Grid item xs={12} sm={6} md={4}><InputLabel width="100%" label="Ism" placeholder="kiriting..."/> </Grid>
+            </Grid>
+            <Container padding="20px 0 5px" textAlign="right">
+                <Button>Saqlash</Button>
+            </Container>
         </Wrapper>
     )
 }
