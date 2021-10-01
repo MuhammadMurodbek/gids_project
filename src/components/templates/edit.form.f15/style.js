a19c575fff8e7f993d18824d4cc9204891e39833
Murodbek <murodbek@thinkland.uz>
2021-10-01 06:35:02 +0500

    sadfF

--- src/components/templates/edit.form.f15/style.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components"
-export const Wrapper = styled.form`
-    width: 100%;
-    padding:30px 0;
-    text-align: center;
-`
--- src/pages/gid.personal.wider/edit/bio/style.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components"
-import {shadow} from "../../../../styles/global/colors"
-export const Wrapper = styled.div`
-    width: 100%;
-    height: auto;
-    /* box-shadow:${shadow}; */
-    /* background-color:red; */
-
-`
