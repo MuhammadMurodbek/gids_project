7af508797ea9ac34987281493251b524a0eb2d4c
Murodbek <murodbek@thinkland.uz>
2021-10-04 15:50:55 +0500

    asdf

--- src/pages/auth/registration/index.js
+++ src/pages/auth/registration/index.js
@@ -1,110 +1,114 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import SwipeableViews from 'react-swipeable-views';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import { Wrapper, Container } from './index.style'
-import Authorization from './auth.component'
-import Registration from "./reg.component"
+import { makeStyles } from "@material-ui/core/styles";
+import * as React from "react";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import { mainGreen } from "../../../styles/global/colors";
+import Authorization from "./auth.component";
+import Registration from "./reg.component";
+import {Wrapper, Container} from "./index.style"
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    width: 500,
+    height: "auto",
+    fontFamily: "Montserrat",
+  },
+  indicator: {
+    backgroundColor: mainGreen,
+  },
+  tab: {
+    color: "red",
+  },
+}));
 
-function TabPanel ( props ) {
-    const { children, value, index, ...other } = props;
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
-    return (
-        <div
-            role="tabpanel"
-            hidden={ value !== index }
-            id={ `full-width-tabpanel-${ index }` }
-            aria-labelledby={ `full-width-tab-${ index }` }
-            { ...other }
-        >
-            {value === index && (
-                <Box p={ 3 }>
-                    <Typography>{ children }</Typography>
-                </Box>
-            ) }
-        </div>
-    );
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
 }
 
 TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.any.isRequired,
-    value: PropTypes.any.isRequired,
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
 };
 
-function a11yProps ( index ) {
-    return {
-        id: `full-width-tab-${ index }`,
-        'aria-controls': `full-width-tabpanel-${ index }`,
-    };
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
 }
 
-const useStyles = makeStyles( ( theme ) => ( {
-    root: {
-        backgroundColor: theme.palette.background.paper,
-        width: 500,
-        height: 'auto',
-        fontFamily:'Montserrat',
-    },
-    indicator: {
-        backgroundColor: "#326A32",
-    },
-    tab:{
-        color:'red'
-    }
-} ) );
+export default function BasicTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
 
-export default function FullWidthTabs () {
-    const classes = useStyles();
-    const theme = useTheme();
-    const [ value, setValue ] = React.useState( 1 );
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-    const handleChange = ( event, newValue ) => {
-        setValue( newValue );
-    };
-
-    const handleChangeIndex = ( index ) => {
-        setValue( index );
-    };
-    return (
-        <Wrapper>
-            <Container>
-                <div className={ classes.root }>
-                    <AppBar position="static" color="default">
-                        <Tabs
-                            value={ value }
-                            onChange={ handleChange }
-                            variant="fullWidth"
-                            TabIndicatorProps={{ className: classes.indicator }}
-                            aria-label="full width tabs example"
-                        >
-                            <Tab label={ <span style={value === 0 ? {color:'#326A32'}:{color:'#666'}} >Kirish</span> } { ...a11yProps( 0 ) } />
-                            <Tab label={ <span style={value === 1 ? {color:'#326A32'}:{color:'#666'}}>Registratsiya</span> } { ...a11yProps( 1 ) } />
-                        </Tabs>
-                    </AppBar>
-                    <SwipeableViews
-                        axis={ theme.direction === 'rtl' ? 'x-reverse' : 'x' }
-                        index={ value }
-                        animateHeight
-                        onChangeIndex={ handleChangeIndex }
+  return (
+      <Wrapper>
+          <Container>
+            <Box sx={{ width: "600px", margin: "0 auto" }}>
+            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                <Tabs
+                style={{ width: "100%" }}
+                value={value}
+                onChange={handleChange}
+                aria-label="basic tabs example"
+                TabIndicatorProps={{ className: classes.indicator }}
+                >
+                <Tab
+                    label={
+                    <span
+                        style={value === 0 ? { color: mainGreen } : { color: "#666" }}
                     >
-                        <div value={ value } index={ 0 } dir={ theme.direction }>
-                            <Authorization/> 
-                        </div>
-                        <div value={ value } index={ 1 } dir={ theme.direction }>
-                            <Registration/> 
-                        </div>
-                    </SwipeableViews>
-                </div>
-            </Container>
-        </Wrapper>
-    );
+                        Kirish
+                    </span>
+                    }
+                    style={{ width: "50%" }}
+                    {...a11yProps(0)}
+                />
+                <Tab
+                    label={
+                    <span
+                        style={value === 1 ? { color: mainGreen } : { color: "#666" }}
+                    >
+                        Registratsiya
+                    </span>
+                    }
+                    style={{ width: "50%" }}
+                    {...a11yProps(1)}
+                />
+                </Tabs>
+            </Box>
+            <TabPanel value={value} index={0}>
+                <Authorization />
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                <Registration />
+            </TabPanel>
+            </Box>
+          </Container>
+      </Wrapper>
+  );
 }
-
-
-
