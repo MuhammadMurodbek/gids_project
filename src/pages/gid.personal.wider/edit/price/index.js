7d8c32662e37c93fde39610a3f7525c7f59f5cde
Murodbek <murodbek@thinkland.uz>
2021-10-05 16:03:48 +0500

    asdf

--- src/pages/gid.personal.wider/edit/price/index.js
+++ src/pages/gid.personal.wider/edit/price/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {Wrapper} from './style';
 import GidPage from "./gid.page"
 import TranslatorPage from "./translator.page"
+import WritePage from "./writer.page"
 const Index = () => {
     return (
         <Wrapper>
             {/* <GidPage/> */}
-            <TranslatorPage/>
+            {/* <TranslatorPage/> */}
+            <WritePage/>
         </Wrapper>
     )
 }
--- src/pages/gid.personal.wider/edit/price/writer.page/index.js
+++ src/pages/gid.personal.wider/edit/price/writer.page/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import {Wrapper} from './style';
-import Input from '../../../../components/atom/input.second';
+import {Container} from "../../../../../styles/container/index.style"
+import PriceCurrency from "./price.currency"
+import SelectLabel from '../../../../../components/molecules/select.labeled'
+import { Grid } from '@mui/material';
+import CheckButton from "../../../../../components/atom/button/custom.check"
+import AddButton from "../../../../../components/atom/button/custom.add"
+import Button from "../../../../../components/atom/button"
 
 const Index = () => {
     return (
         <Wrapper>
-           <div className="narxlar">
-                <p>Bir kunlik xizmatddddd narxi</p>
-                <Input width="90px" />
-           </div>
+           <Container>
+               <Grid container spacing={1} justifyContent="space-between" > 
+                    <Grid alignSelf="center" justifySelf="center" item xs={12} md={5} lg={4}><SelectLabel width="100%" label="Tilni tanlang" placeholder="English->Uzbek"/></Grid>
+                    <Grid alignSelf="center" justifySelf="center" item xs={12} md={5} lg={3}><PriceCurrency title="1 sahifa uchun - bo'shliqlar bilan 1800 belgi"/></Grid>
+                    <Grid alignSelf="center" justifySelf="center" item xs={12} md={5} lg={3}><PriceCurrency title="1 bet uchun - 1800 belgi bo'sh joysiz"/></Grid>
+                    {/* <Grid alignSelf="center" justifySelf="center" item xs={12} md={4} lg={2}><SelectLabel width="100%" label="Kuniga necha soat" placeholder="24"/></Grid> */}
+                    <Grid alignSelf="flex-end" justifySelf="flex-start" item xs={12} md={1} lg={1}><CheckButton type="outliled" positionEffect={{position:'relative',top:"3px"}}/></Grid>
+               </Grid>
+           </Container>
+           <Container>
+               <Grid container spacing={1} justifyContent="space-between" >
+                    <Grid item xs={12} md={1} lg={1}></Grid>
+                    <Grid alignSelf="flex-end" justifySelf="center" item xs={12} md={1} lg={1}><AddButton/></Grid>
+               </Grid>
+           </Container>
+           <Container width="100%" padding="10px 20px" margin="20px 0 0 0" textAlign="right">
+                <Button>Saqlash</Button>
+           </Container>
         </Wrapper>
     )
 }
