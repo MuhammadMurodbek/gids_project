629423db8fe63a55bf73553c59b1b38e98c047e5
Abdurahmon7711 <Abdurahmon1177@gmail.com>
2021-10-31 11:38:35 +0500

    asdfasf

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
 import Layout from "../layouts/main"
-import {routes} from "../routes/router.list"
+import {check_route} from "../routes/router.list"
 import ToastCNT from "../components/atom/toast.container"
-// import { useLocation } from 'react-router-dom';
 const Index = () => {
-
-    localStorage.setItem("role", "simple_user")
-
     return (
         <Router>
             <ToastCNT/>
             <Layout>
                 <Switch>
                     <Route exact path="/"><Redirect to="/main" /></Route>
-                    {routes.map((item,index)=>(
+                    {check_route('translator').map((item,index)=>(
                         <Route key={index} exact path={item.url} component={item.component}/>
                     ))}
                 </Switch>
