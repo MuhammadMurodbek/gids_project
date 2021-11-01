d4d17eaa9aa6ad329a271ff1b8b602255fabbec0
Murodbek <murodbek@thinkland.uz>
2021-11-01 22:07:54 +0500

    adsfadsF

--- src/components/molecules/img.container/index.styled.js
+++ src/components/molecules/img.container/index.styled.js
@@ -4,12 +4,14 @@ export const ImgWrapper = styled.div`
     width: ${value=>value.width ? value.width:"100%"};
     height: auto;
     text-align: ${value=>value.align ? value.align: 'center'};
+    /* background-color: red;/ */
+    margin:0 auto;
     img{
         width: ${value=>value.width};
         height: ${value=>value.height ? value.height : "auto"};
         object-fit: cover;
         border-radius: ${value=>value.radius ? value.radius : "none"};
-        margin:${value=>value.margin ? value.margin : "0"}
+        margin:${value=>value.margin}
     }
     @media (max-width:${a=>a.media}){
         width: ${a=>a.m_m_width};
