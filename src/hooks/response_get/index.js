fa9287468e2ecc84b03fb99790c31b3e4634a796
Murodbek <murodbek@thinkland.uz>
2021-11-18 22:56:42 +0500

    asdfS

--- src/hooks/response_get/index.js
+++ src/hooks/response_get/index.js
@@ -13,8 +13,8 @@ export const getResponseRegion = async(url) => {
 }
 export const postResponse = async(url, data, setState) => {
     return await axios.post(`${baseUrl}${url}`,data, head_token)
-        .then(response => setState ({success: response, error:''}))
-        .catch(err => setState ({success:'',error: err}))
+        .then(response => setState ({success: response, error:'', loading: false}))
+        .catch(err => setState ({success:'',error: err, loading: false}))
 }
 export const putResponse = async(url, data, setState) => {
     return await axios.put(`${baseUrl}${url}`,data, head_token)
