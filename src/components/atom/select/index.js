47634ece3e59c09a94dc9676a9724496e73d49a0
Murodbek <murodbek@thinkland.uz>
2021-11-21 19:26:58 +0500

    asdfadsf

--- src/components/atom/select/index.js
+++ src/components/atom/select/index.js
@@ -18,6 +18,7 @@ const Index = (props) => {
     clearValue,
     setClearValue,
     pcolor,
+    defaultApiValue
   } = props;
   const [value, setValue] = useState(null);
   const handleChange = useCallback(
@@ -43,6 +44,11 @@ const Index = (props) => {
         setValue(null)
       }
     },[clearValue])
+    useEffect(() => {
+      if(defaultApiValue){
+        setValue({value:0, label:defaultApiValue})
+      }
+    },[defaultApiValue])
   return (
     <Wrapper
       width={width}
