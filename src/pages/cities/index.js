f2cc912f7f3d4c9736b8f1876b1d6fa339593e2b
Abdurahmon7711 <Abdurahmon1177@gmail.com>
2021-11-25 01:16:13 +0500

    s

--- src/pages/cities/index.js
+++ src/pages/cities/index.js
@@ -9,7 +9,7 @@ import ad from '../../assets/img/citiyes/ad.png'
 import xitoy from '../../assets/img/citiyes/xitoy.png';
 import { mediaTextField, mediaTextFieldSec } from "../../custom/global.media.variables"
 import { useTranslation } from 'react-i18next'
-
+import Blog from '../blog'
 
 export default function Index() {
 
@@ -36,64 +36,9 @@ export default function Index() {
                 </AboutB>
             </PerentAB>
 
-            <TextTitle {...mediaTextField} {...mediaTextFieldSec} width="100%" left="auto" right="auto" top="10px">
-                Blog
-            </TextTitle>
-
-            <Grid justifyContent="center" container spacing={1}>
-                <Grid item xs={11} sm={11} md={7}>
-
-                    <CitiLeftPart
-                        title="Xitoyga sayoxat"
-                        kalendar="23.01.2021"
-                        hteg="Biznes, Xitoy"
-                        url={xitoy}
-
-                        text=" lor sit amet, consectetur adipiscing elit. Ac tristique volutpat commodo, sed risus, vitae. Habitant donec pellentesque et facilisis urna. Venenatis, mauris elementum netus morbi. Elit ultricies nisi, maecenas turpis pulvinar ultrices. Eros, placerat lacinia diam lorem ultrices sem ullamcorper blandit lorem. Urna sodales dictumst amet, ornare et, morbi quis ut arcu. Ac tortor senectus consequat vulputate eleifend magna. Aliquam quis faucibus in tincidunt lacus, fusce. Sagittis accumsan, amet risus natoque eu quis dictum. Egestas nisl quis maecenas euismod. Ultrices eget feugiat c. Donec metus, senectus a convallis quis scelerisque. Risus magna tellus ante sollicitudin vehicula quis commodo scelerisque ac. Consequat ullamcorper ullamcorper volutpat orci urna potenti velit. Sit dictum scelerisque commodo, in ut. Feugiat nunc cursus a gravida pulvinar aliquet varius vitae. Quam sed cursus quisque mauris vestibulum sem pulvinar urna. Habitasse ante enim rhoncus, facilisi arcu sagittis, sed. Curabitur nibh imperdiet enim est dui..."
-                        btnText={t("Blog.davomiOqish")}
-                    />
-
-                    <CitiLeftPart
-                        title="Xitoyga sayoxat"
-                        kalendar="23.01.2021"
-                        hteg="Biznes, 
-                        Xitoy"
-                        lines="10"
-                        url={xitoy}
-                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac tristique volutpat commodo, sed risus, vitae. Habitant donec pellentesque et facilisis urna. Venenatis, mauris elementum netus morbi. Elit ultricies nisi, maecenas turpis pulvinar ultrices. Eros, placerat lacinia diam lorem ultrices sem ullamcorper blandit lorem. Urna sodales dictumst amet, ornare et, morbi quis ut arcu. Ac tortor senectus consequat vulputate eleifend magna. Aliquam quis faucibus in tincidunt lacus, fusce. Sagittis accumsan, amet risus natoque eu quis dictum. Egestas nisl quis maecenas euismod. Ultrices eget feugiat bibendum fringilla.
-                        Magna nam est morbi molestie vel nunc. Donec metus, senectus a convallis quis scelerisque. Risus magna tellus ante sollicitudin vehicula quis commodo scelerisque ac. Consequat ullamcorper ullamcorper volutpat orci urna potenti velit. Sit dictum scelerisque commodo, in ut. Feugiat nunc cursus a gravida pulvinar aliquet varius vitae. Quam sed cursus quisque mauris vestibulum sem pulvinar urna. Habitasse ante enim rhoncus, facilisi arcu sagittis, sed. Curabitur nibh imperdiet enim est dui..."
-                        btnText={t("Blog.davomiOqish")}
-                    />
-
-                    <CitiLeftPart
-                        title="Xitoyga sayoxat"
-                        kalendar="23.01.2021"
-                        hteg="Biznes, Xitoy"
-                        url={xitoy}
-                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac tristique volutpat commodo, sed risus, vitae. Habitant donec pellentesque et facilisis urna. Venenatis, mauris elementum netus morbi. Elit ultricies nisi, maecenas turpis pulvinar ultrices. Eros, placerat lacinia diam lorem ultrices sem ullamcorper blandit lorem. Urna sodales dictumst amet, ornare et, morbi quis ut arcu. Ac tortor senectus consequat vulputate eleifend magna. Aliquam quis faucibus in tincidunt lacus, fusce. Sagittis accumsan, amet risus natoque eu quis dictum. Egestas nisl quis maecenas euismod. Ultrices eget feugiat bibendum fringilla.
-                        Magna nam est morbi molestie vel nunc. Donec metus, senectus a convallis quis scelerisque. Risus magna tellus ante sollicitudin vehicula quis commodo scelerisque ac. Consequat ullamcorper ullamcorper volutpat orci urna potenti velit. Sit dictum scelerisque commodo, in ut. Feugiat nunc cursus a gravida pulvinar aliquet varius vitae. Quam sed cursus quisque mauris vestibulum sem pulvinar urna. Habitasse ante enim rhoncus, facilisi arcu sagittis, sed. Curabitur nibh imperdiet enim ."
-                        btnText={t("Blog.davomiOqish")}
-                    />
-
-
-
-                </Grid>
-
-                <Grid className="msa2" item xs={12} sm={12} md={4}>
-                    <div className="addsa">
-                        <ImgContainer src={ad} />
-                    </div>
-                    <div className="addsa">
-                        <ImgContainer src={ad} />
-                    </div>
-                    <div className="addsa">
-                        <ImgContainer src={ad} />
-                    </div>
-
-
-
-                </Grid>
-            </Grid>
+            
+                 <Blog/>
+             
 
         </Wrapper>
     )
