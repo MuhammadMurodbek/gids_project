303ff24f21522e51e4697410aee0ac1a826fdb95
Murodbek <murodbek@thinkland.uz>
2021-11-24 12:42:57 +0500

    ADSfadsf

--- src/components/extended.search/index.js
+++ src/components/extended.search/index.js
@@ -9,20 +9,25 @@ import Button from "./button.component"
 import DoubleRadio from "../molecules/double.radio.labeled"
 import {useTranslation} from 'react-i18next'
 import {getResponse} from "../../hooks/response_get"
+import {toast} from "react-hot-toast"
 const Index = () => {
    
     const {t} = useTranslation() 
-    const [postData, setPostData] = useState()
+    const [postData, setPostData] = useState({ success: '', error: '', loading: false})
     const [collect, setCollect] = useState()
     const handleSubmit = () => {
-        getResponse(`/api/gids/profiles/`, setPostData)
+        console.log('sadf')
+        let url = `/api/${collect.type}s/profiles/?gender=${collect?.male ? 'male' : 'female'}&country=${collect?.country?.label}&city=${collect?.city?.label}&lang=${collect?.lang?.map(item=>item.label)}&date_after=${collect?.date_after}&date_before=${collect?.date_before}&${collect?.gender}=0`
+        getResponse(url, setPostData)
     }
-    console.log(collect)
+    React.useEffect( () => {
+        if(postData?.error !== '') toast.error("Ma'lumotlarni to'liq kiriting")
+    },[postData])
     return (    
         <Wrapper width="350px">
             <div className="title-header">{t("kengaytirlgan_Q.kengaytirilgan")}</div>
 
-            <RadioGroup setState={setCollect} state={collect} field='gender'/>   
+            <RadioGroup setState={setCollect} state={collect} field='type'/>   
 
             <Selection setState={setCollect} state={collect} field1='country' field2='city' title={t("kengaytirlgan_Q.Davlat")} placeholder={t("kengaytirlgan_Q.DavlatniTanlang")}/>
 
@@ -37,7 +42,7 @@ const Index = () => {
             <DoubleRadio value1="online" value2="all"  setState={setCollect} state={collect} name1={t("kengaytirlgan_Q.online")} name2={t("kengaytirlgan_Q.barchasi")}/>
             
             <div className="button-wrapper">
-                <Button width="280px" name={t("kengaytirlgan_Q.qidirish")}/>
+                <Button onClick={handleSubmit} width="280px" name={t("kengaytirlgan_Q.qidirish")}/>
             </div>
         </Wrapper>
     )
