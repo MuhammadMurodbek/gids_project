23945ec798a9b6636482e4274d1ca188138bc195
Murodbek <murodbek@thinkland.uz>
2021-11-25 15:00:28 +0500

    Adsfas

--- src/pages/gid.personal.wider/calendar/index.js
+++ src/pages/gid.personal.wider/calendar/index.js
@@ -1,10 +1,11 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import {Wrapper,DayPickerWrapper, ButtonWrapper} from "./style"
 import { Container } from '../../../styles/container/index.style'
 import DayPicker from "../../../components/molecules/daypicker"
 import { Switch } from 'antd';
 import {mainGreen} from "../../../styles/global/colors"
 import { Grid } from '@mui/material'
+import Button from "../../../components/atom/button"
 const mediaContainer = {
     m_width:'740px',
     m_m_width:'300px',
@@ -13,8 +14,8 @@ const Index = () => {
     const [dateState, setDateState] = useState([])
     console.log(dateState)
     return (
+        <>
         <Wrapper>
-          
             <Container {...mediaContainer} width="600px" margin="0 auto" padding="0" className="container4" textAlign="center">
                 <Grid container spacing={0} justifyContent="center" alignItems="center" flexDirection="column">
                     <Grid item xs={12} md={6}>
@@ -46,15 +47,15 @@ const Index = () => {
                             <DayPicker title="no" setData={setDateState}/>
                         </DayPickerWrapper>
                     </Grid>
-                    <Grid item xs={12} md={6}>
-                        <ButtonWrapper>
-                            {/* <button><img src={RemoveCircleOutlineIcon} alt="a"/> &nbsp;&nbsp; Band qilish uchun</button>
-                            <button><img src={CheckCircleOutlineIcon} alt="d"/> &nbsp;&nbsp; Bosh qilish uchun</button> */}
-                        </ButtonWrapper>
+                    <Grid item xs={12} md={6} style={{marginTop: 15}}>
+                        <Button type="outlined" style={{width:"300px"}}>Saqlash</Button>
                     </Grid>
                 </Grid>
             </Container>
         </Wrapper>
+        {/* <Container width="90%" padding="10px" textAlign="right">
+        </Container> */}
+        </>
     )
 }
 
