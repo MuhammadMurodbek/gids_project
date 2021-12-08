621c4b21efd7fbc0348b6c60d5da7119a7b61d86
Murodbek <murodbek@thinkland.uz>
2021-12-08 22:34:54 +0500

    ASdfsa

--- src/pages/gid.personal.wider/edit/price/translator.page/index.js
+++ src/pages/gid.personal.wider/edit/price/translator.page/index.js
@@ -1,50 +1,59 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import { Wrapper } from './style';
 import { Container } from "../../../../../styles/container/index.style"
 import PriceCurrency from "./price.currency"
 import SelectLabel from '../../../../../components/molecules/select.labeled'
 import { Grid } from '@mui/material';
-import RemoveButton from "../../../../../components/atom/button/custom.remove"
-import DeleteIcon from '@material-ui/icons/Delete';
-import AddIcon from '@material-ui/icons/Add';
-import AddButton from "../../../../../components/atom/button/custom.add"
+import TextLabeledLoop from "../../../../../components/atom/text.labeled"
+import {getResponse} from "../../../../../hooks/response_get"
 import Button from "../../../../../components/atom/button"
-
+import { common } from "../../../../../custom/url"
+import Spinner from "../../../../../components/atom/loading.spinner.line";
+import {getLabelLangLocal} from "../../../../../custom/function"
+import {hours} from "./_const"
 const Index = () => {
+    const [getData, setGetData] = useState( { success: '', error: ''})
+    const [postCollect, setPostCollect] = useState([])
+    useEffect( () => { getResponse( common.personal.edit.language, setGetData ) }, [ ] )
+    useEffect(()=>{
+        if(getData.success!==''){
+            setItems(getData?.success?.data)
+            setPostCollect(getData?.success?.data?.map(a=>{return{id:a.id}}))
+        }
+    },[getData])
+    const [items, setItems] = useState([])
+    const [item, setItem] = useState({cost_per_hour: 0,currency_per_hour: {},cost_per_day: 0,currency_per_day: {},work_time_per_day: 0})
+    useEffect(()=>{
+        let clone = postCollect
+        clone[item?.idK-1] = item
+        setPostCollect(clone)
+    },[item])
+    console.log(postCollect)
     return (
         <Wrapper>
-            <Container>
-                <Grid container spacing={1} justifyContent="space-between"  alignItems="center">
-                    <Grid item xs={12} md={4} lg={3}>
-                        <SelectLabel width="100%" label="Tilni tanlang" placeholder="English->Uzbek" />
-                    </Grid>
-                    <Grid item xs={12} md={2} lg={3}>
-                        <PriceCurrency title="Bir soatlik xizmat narxi" />
-                    </Grid>
-                    <Grid item xs={12} md={2} lg={3}>
-                        <PriceCurrency title="Bir kunlik xizmat narxi" />
-                    </Grid>
-                    <Grid item xs={12} md={2} lg={2}>
-                        <SelectLabel width="100%" label="Kuniga necha soat" placeholder="24" />
-                    </Grid>
-                    <Grid itme md={2}>
-                        <Button paddingIcon="10px" type="outlined">
-                            <DeleteIcon className="icon" />
-                        </Button>
-                    </Grid>
-                </Grid>
-            </Container>
-
-            <Container>
-                <Grid container spacing={1} justifyContent="space-between" >
-                    <Grid item xs={12} md={1} lg={1}></Grid>
-                    <Grid alignSelf="flex-end" justifySelf="center" item xs={12} md={1} lg={1}>
-                        <Button paddingIcon="10px"  >
-                            <AddIcon className="icon" />
-                        </Button>
-                    </Grid>
-                </Grid>
-            </Container>
+            {
+                 getData?.success === '' ? <Spinner marginTop="60px" width={ 50 } height={ 50 } /> :
+                 items?.map((prev, index)=>(
+                    <Container key={ index }>
+                        <Grid container spacing={1} justifyContent="space-between"  alignItems="center">
+                            <Grid item xs={12} sm={ 6 } md={4} lg={3}>
+                                <TextLabeledLoop label="Tillar" value={(getLabelLangLocal(prev?.from_language)+"->"+getLabelLangLocal(prev?.to_language) || null)}/>
+                            </Grid>
+                            <Grid item xs={12} sm={ 6 } md={3} lg={3}>
+                                <PriceCurrency setState={setItem} state={item} idK={index+1} fieldS='currency_per_hour' fieldIn="cost_per_hour" title="Bir soatlik xizmat narxi" />
+                            </Grid>
+                            <Grid item xs={12} sm={ 6 } md={3} lg={3}>
+                                <PriceCurrency setState={setItem} state={item} idK={index+1} fieldS='currency_per_day' fieldIn="cost_per_day" title="Bir kunlik xizmat narxi" />
+                            </Grid>
+                            <Grid item xs={12} sm={ 6 } md={2} lg={2}>
+                                <SelectLabel options={hours} field="work_time_per_day" setCollect={setItem} collect={item} idK={index+1} width="100%" label="Kuniga necha soat" placeholder="24" />
+                            </Grid>
+                        </Grid>
+                        
+                    </Container>
+                 ))                
+                
+            }
             <Container width="100%" padding="10px 20px" margin="20px 0 0 0" textAlign="right">
                 <Button>Saqlash</Button>
             </Container>
