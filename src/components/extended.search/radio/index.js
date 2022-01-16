6cb527a4780f30a778e931c52e2a9b5ef5875ff4
Murodbek <murodbek@thinkland.com>
2022-01-16 13:43:18 +0500

    oops

--- src/components/extended.search/radio/index.js
+++ src/components/extended.search/radio/index.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React from 'react'
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
@@ -7,10 +7,10 @@ import {Wrapper} from "./index.style"
 import DoubleCheck2 from "../../molecules/doble2"
 import { useTranslation } from 'react-i18next';
 
-const Index = ({setState, state, field, setWriter}) => {
+const Index = ({setState, state, field}) => {
     const {t} = useTranslation()
     const [value, setValue] = React.useState(null);
-    const [checkValue, setCheckValue] = useState(null)
+
     const handleChange = (event) => {
         setValue(event.target.value);
         if(setState){
@@ -18,12 +18,7 @@ const Index = ({setState, state, field, setWriter}) => {
         }
         console.log(event.target.value)
     }
-    // React.useEffect(()=>{
-    //     if(checkValue){
-    //         setWriter({is_writer:checkValue.is_writer})
-    //     }
-    // },[checkValue])
-    // console.log(checkValue)
+    // console.log(value)
     return (
         <Wrapper>
             <div className="title-header-radio"> {t("kengaytirlgan_Q.kimKerak")}</div>
@@ -33,15 +28,12 @@ const Index = ({setState, state, field, setWriter}) => {
                 <FormControlLabel value="translator" control={<Radio />} label={<span>{t("kengaytirlgan_Q.tarjion")}</span>} />
                 <div style={value === "translator" ? {display:"inline-block"}:{display:'none'}}>
                     <DoubleCheck2 margin="0 0 0 30px" 
-                        name1={t("kengaytirlgan_Q.sinxron")} 
-                        name2={t("kengaytirlgan_Q.izchil")} 
-                        name3={t("kengaytirlgan_Q.yozmaTarjimon")}
-                        flexDirection="column"
-                        collect={checkValue}
-                        setCollect={setCheckValue}
-                        setWriter={setWriter}
-                    />
+                    name1={t("kengaytirlgan_Q.sinxron")} 
+                    name2={t("kengaytirlgan_Q.izchil")} 
+                    name3={t("kengaytirlgan_Q.yozmaTarjimon")}
+                    flexDirection="column"/>
                 </div>
+                
             </RadioGroup>
             </FormControl>
         </Wrapper>
