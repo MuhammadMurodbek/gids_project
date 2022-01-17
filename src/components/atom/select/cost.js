732013cf1f8e643d8690be14e2672162b5ddcdf1
Murodbek <murodbek@thinkland.com>
2022-01-17 06:17:31 +0500

    asdfdsf

---
+++ src/components/atom/select/cost.js
@@ -0,0 +1,100 @@
+import React, { useState, useCallback, useEffect } from "react";
+import Select from "react-select";
+import { Wrapper } from "./index.style";
+import "./style.css";
+import {CURRENCY_CHECK} from "../../../custom/constants"
+const Index = (props) => {
+  const {
+    width,
+    paddingX,
+    margin,
+    backgroundColor,
+    setState,
+    state,
+    errorText,
+    setCollect,
+    collect,
+    field,
+    clearValue,
+    setClearValue,
+    pcolor,
+    defaultApiValue,
+    idK,
+    prev,
+  } = props;
+  const [value, setValue] = useState(null);
+  const handleChange = 
+    (valueChange) => {
+      console.log(valueChange)
+      setValue(valueChange);
+      setCollect({idK:idK, value:valueChange, field:[field]})
+    //   if (setState) {
+    //     setState(valueChange);
+    //   }
+    //   if (setCollect) {
+    //     if(idK){
+    //       setCollect((prev) => {
+    //         return { ...prev, [field]: valueChange, idK: idK};
+    //       });
+    //     }else{
+    //       setCollect((prev) => {
+    //         return { ...prev, [field]: valueChange };
+    //       });
+    //     }
+    //   } ///important
+    //   if(setClearValue){
+    //     setClearValue(false);
+    //   }
+    }
+    useEffect(() => {
+      if(clearValue){
+        setValue(null)
+      }
+    },[clearValue])
+    useEffect(() => {
+      if(field && field === 'work_time_per_day'){
+        if(prev.hasOwnProperty('value'))
+          setValue(prev)
+        else
+          setValue({value:prev,label:prev})
+      }else{
+        if(prev.hasOwnProperty('value')){ 
+          setValue(prev)
+        }else{
+          setValue({value:prev,label:CURRENCY_CHECK[prev]})
+        }
+      }
+    },[prev, field])
+    
+  return (
+    <Wrapper
+      width={width}
+      paddingX={paddingX}
+      margin={margin}
+      backgroundColor={backgroundColor}
+      pcolor={pcolor}
+    >
+      <Select
+        {...props}
+        value={value}
+        onChange={handleChange}
+        menuPortalTarget={document.body}
+        styles={{ menuPortal: (base) => ({ ...base, zIndex: 9999 }) }}
+      />
+      <span
+        style={{
+          position: "absolute",
+          fontSize: "11px",
+          left: 0,
+          bottom: -18,
+          fontStyle: "italic",
+          color: "red",
+        }}
+      >
+        {errorText}
+      </span>
+    </Wrapper>
+  );
+};
+
+export default Index;
--- src/pages/gid.personal.wider/edit/price/translator.page/index.js
+++ src/pages/gid.personal.wider/edit/price/translator.page/index.js
@@ -2,7 +2,7 @@ import React,{useEffect, useState} from 'react';
 import { Wrapper } from './style';
 import { Container } from "../../../../../styles/container/index.style"
 import PriceCurrency from "./price.currency"
-import SelectLabel from '../../../../../components/molecules/select.labeled'
+import SelectLabel from '../../../../../components/molecules/select.labeled/cost'
 import { Grid } from '@mui/material';
 import TextLabeledLoop from "../../../../../components/atom/text.labeled"
 import {getResponse, postResponse} from "../../../../../hooks/response_get"
@@ -17,37 +17,53 @@ import {saveTabAction} from "../../../../../redux/actions"
 const Index = () => {
     const dispatch = useDispatch()
     const [getData, setGetData] = useState( { success: '', error: ''})
+    const [stateCurr, setStateCurr] = useState({idK:null, currency:null})
+    const [items, setItems] = useState([])
     const [postCollect, setPostCollect] = useState([])
+    const [item, setItem] = useState(null)
     const [post, setPost] = useState( { success: '', error:'', loading: false})
-    useEffect( () => { getResponse( common.personal.edit.language, setGetData ) }, [ ] )
+    useEffect( () => { getResponse( common.personal.edit.cost, setGetData ) }, [ ] )
     useEffect(()=>{
         if(getData.success!==''){
             setItems(getData?.success?.data)
-            setPostCollect(getData?.success?.data?.map(a=>{return{id:a.id}}))
+            setPostCollect(getData?.success?.data)
         }
     },[getData])
-    const [items, setItems] = useState([])
-    const [item, setItem] = useState({cost_per_hour: 0,currency_per_hour: {},cost_per_day: 0,currency_per_day: {},work_time_per_day: 0})
+    // useEffect(()=>{
+    //     if(item.hasOwnProperty('idK'))
+    //     setItem(items[item.idK-1])
+    // },[item.idK])
     useEffect(()=>{
-        if(postCollect.length > 0){
-            let clone = postCollect
-            clone[item?.idK-1] = {...item, id:clone[item?.idK-1]?.id}
-            setPostCollect(clone)
-        }
-    },[item])
+        // console.log(postCollect)
+        // console.log(stateCurr)
+        // console.log(item)
+        let clone = items
+        if(item?.idK) clone[item?.idK-1][item?.field] = item?.value
+        if(stateCurr?.idK) clone[stateCurr?.idK-1][stateCurr?.field] = stateCurr?.value
+        setItems(clone)
+        
+        // console.log(clone)
+        // if(postCollect.length > 0){
+        //     let clone = postCollect
+        //     clone[item?.idK-1] = {...items[item.idK-1],  ...item}
+        //     setPostCollect(clone)
+        // }
+    },[item, stateCurr])
+    // console.log(stateCurr)
     const handleSubmit = ()=>{
         setPost({...post, loading: true})
-        let postData = postCollect?.map(item=>{
+        console.log(items)
+        let postData = items?.map((prev)=>{
             return{
-                id:item?.id,
-                cost_per_day:parseInt(item?.cost_per_day),
-                cost_per_hour:parseInt(item?.cost_per_hour),
-                currency_per_day:item?.currency_per_day?.value,
-                currency_per_hour:item?.currency_per_hour?.value, 
-                work_time_per_day:item?.work_time_per_day?.value,
+                id:prev?.id,
+                cost_per_day:parseFloat(prev?.cost_per_day),
+                cost_per_hour:parseFloat(prev?.cost_per_hour),
+                currency_per_day:prev?.currency_per_day?.value || prev?.currency_per_day,
+                currency_per_hour:prev?.currency_per_hour?.value || prev?.currency_per_hour, 
+                work_time_per_day:prev?.work_time_per_day?.value  || prev?.work_time_per_day,
             }
         })
-        // console.log(postData)
+        console.log(postData)
         postResponse('/api/translators/edit/cost/', postData, setPost)
     }
     useEffect(()=>{
@@ -66,13 +82,13 @@ const Index = () => {
                                 <TextLabeledLoop label="Tillar" value={(getLabelLangLocal(prev?.from_language)+"->"+getLabelLangLocal(prev?.to_language) || null)}/>
                             </Grid>
                             <Grid item xs={12} sm={ 6 } md={3} lg={3}>
-                                <PriceCurrency setState={setItem} state={item} idK={index+1} fieldS='currency_per_hour' fieldIn="cost_per_hour" title="Bir soatlik xizmat narxi" />
+                                <PriceCurrency prev={prev} setStateCurr={setStateCurr} stateCurr={stateCurr} defaultCost={prev?.cost_per_hour} defaultCurr={prev?.currency_per_hour} setState={setItem} state={item} idK={index+1} fieldS='currency_per_hour' fieldIn="cost_per_hour" title="Bir soatlik xizmat narxi" />
                             </Grid>
                             <Grid item xs={12} sm={ 6 } md={3} lg={3}>
-                                <PriceCurrency setState={setItem} state={item} idK={index+1} fieldS='currency_per_day' fieldIn="cost_per_day" title="Bir kunlik xizmat narxi" />
+                                <PriceCurrency prev={prev} setStateCurr={setStateCurr} stateCurr={stateCurr} defaultCost={prev?.cost_per_day} defaultCurr={prev?.currency_per_day} setState={setItem} state={item} idK={index+1} fieldS='currency_per_day' fieldIn="cost_per_day" title="Bir kunlik xizmat narxi" />
                             </Grid>
                             <Grid item xs={12} sm={ 6 } md={2} lg={2}>
-                                <SelectLabel options={hours} field="work_time_per_day" setCollect={setItem} collect={item} idK={index+1} width="100%" label="Kuniga necha soat" placeholder="24" />
+                                <SelectLabel prev={prev['work_time_per_day']} options={hours} field="work_time_per_day" setCollect={setItem} collect={item} idK={index+1} width="100%" label="Kuniga necha soat" placeholder="24" />
                             </Grid>
                         </Grid>
                         
