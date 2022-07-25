dd05efd08935a1a634b40e051cfb6bb7dd622f3d
StiLKnoT <stilknot@gmail.com>
2022-07-25 16:25:17 +0500

    sd

--- src/hooks/traslator/index.js
+++ src/hooks/traslator/index.js
@@ -1,7 +1,6 @@
 import { useTranslation } from "react-i18next";
 
 function useTraslation01(data) {
-    console.log('komiljonov');
     window.close();
     const {t} = useTranslation();
     return {translator}
