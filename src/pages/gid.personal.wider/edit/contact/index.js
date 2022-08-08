047bb24405d0306d87e0c8d50642abe7c97fed04
a.sulaymonov1177@gmail.com <Abdurahmon1177@gmail.com>
2022-08-08 09:59:06 +0500

    push

--- src/pages/gid.personal.wider/edit/contact/index.js
+++ src/pages/gid.personal.wider/edit/contact/index.js
@@ -18,18 +18,20 @@ const Index = () => {
     const [apiResponse, setApiResponse] = useState({ success: '', error: '' })
     useEffect(() => { getResponse(`/api/${getRole?.role}s/edit/contact/`, setApiResponse) }, [])
     const [apiRes, setApiRes] = useState({ success: '', error: '', loading: false })
-    const [state, setState] = useState({ website: '', telegram: '', instagram: '', facebook: '', wechat: '', viber: '' })
+    const [state, setState] = useState({ telegram: '', instagram: '', facebook: '', wechat: '', viber: '' })
+    const [afterdata,setAfterData] = useState({})
     const handleSubmit = (e) => {
         e.preventDefault()
         let postState = {
-            website: state?.website ? 'https://whatsapp.com/' + state?.website : '',
-            telegram: state?.telegram ? 'https://t.me/' + state?.telegram : '',
-            instagram: state?.instagram ? 'https://instagram.com/' + state?.instagram : '',
-            facebook: state?.facebook ? 'https://facebook.com/' + state?.facebook : '',
-            wechat: state?.wechat ? 'https://wechat.com/' + state?.wechat : '',
-            viber: state?.viber ? 'https://viber.com/' + state?.viber : ''
+            // website: state?.website ? 'https://whatsapp.com/' + state?.website : '',
+            telegram: state?.telegram ? 'https://t.me/' + state?.telegram : apiResponse?.success?.data?.telegram,
+            instagram: state?.instagram ? 'https://instagram.com/' + state?.instagram : apiResponse?.success?.data?.instagram,
+            facebook: state?.facebook ? 'https://facebook.com/' + state?.facebook : apiResponse?.success?.data?.facebook,
+            wechat: state?.wechat ? 'https://wechat.com/' + state?.wechat : apiResponse?.success?.data?.wechat,
+            viber: state?.viber ? 'https://viber.com/' + state?.viber :apiResponse?.success?.data?.viber
         }
         putResponse(`/api/${getRole?.role}s/edit/contact/`, JSON.parse(JSON.stringify(postState)), setApiRes)
+        setAfterData(apiResponse?.success?.data)
     }
     useEffect(() => {
         if (apiRes?.success !== "") {
@@ -39,14 +41,12 @@ const Index = () => {
         } else if (apiRes?.error !== "") {
             toast.error(t("kommentlar.xatolikMavjud"))
         }
+        
     }, [apiRes])
 
-    console.log(apiResponse?.success?.data?.website);
-console.log(apiResponse?.success?.data?.telegram);
-console.log(apiResponse?.success?.data?.instagram);
-console.log(apiResponse?.success?.data?.facebook);
-console.log(apiResponse?.success?.data?.wechat);
-console.log(apiResponse?.success?.data?.viber);
+console.log(apiResponse?.success?.data);
+// console.log(afterdata + "yangi qo'shilganlar +++++++++++++++++++");
+ 
 
     return (
         <Wrapper>
@@ -54,9 +54,9 @@ console.log(apiResponse?.success?.data?.viber);
                 apiResponse?.success === '' ? <Spinner marginTop="60px" width={50} height={50} /> : (
                     <>
                         <Grid container spacing={2}>
-                            <Grid item xs={12} sm={6} md={4}>
+                            {/* <Grid item xs={12} sm={6} md={4}>
                                 <InputLabel log="https://whatsapp.com/" setState={setState} state={state} field="website"   defaultApiValue={apiResponse?.success?.data?.website?.substr(21)}   sizeLabel="15px" width="100%" label="whatsapp" placeholder="https://www.whatsapp.com/user-name" />
-                            </Grid> 
+                            </Grid>  */}
                             <Grid item xs={12} sm={6} md={4}>
                                 <InputLabel log="https://t.me/"         setState={setState} state={state} field="telegram"  defaultApiValue={apiResponse?.success?.data?.telegram?.slice(13)}  sizeLabel="15px" width="100%" label="Telegram" placeholder="https://t.me/user-name" />
                             </Grid>
@@ -67,10 +67,10 @@ console.log(apiResponse?.success?.data?.viber);
                                 <InputLabel log="https://facebook.com/" setState={setState} state={state} field="facebook"  defaultApiValue={apiResponse?.success?.data?.facebook?.slice(21)}  sizeLabel="15px" width="100%" label="Facebook" placeholder="https://www.facebook.com/user_name" />
                             </Grid>
                             <Grid item xs={12} sm={6} md={4}>
-                                <InputLabel log="https://viber.com/"    setState={setState} state={state} field="viber"     defaultApiValue={apiResponse?.success?.data?.viber?.slice(18)}     sizeLabel="15px" width="100%" label="Viber" placeholder="https://viber.me/user-name" />
+                                <InputLabel log="https://wechat.com/"   setState={setState} state={state} field="wechat"    defaultApiValue={apiResponse?.success?.data?.wechat?.slice(19)}    sizeLabel="15px" width="100%" label="Wechat" placeholder="https://spacekid.me/user-name" />
                             </Grid>
                             <Grid item xs={12} sm={6} md={4}>
-                                <InputLabel log="https://wechat.com/"   setState={setState} state={state} field="wechat"    defaultApiValue={apiResponse?.success?.data?.wechat?.slice(19)}    sizeLabel="15px" width="100%" label="Wechat" placeholder="https://spacekid.me/user-name" />
+                                <InputLabel log="https://viber.com/"    setState={setState} state={state} field="viber"     defaultApiValue={apiResponse?.success?.data?.viber?.slice(18)}     sizeLabel="15px" width="100%" label="Viber" placeholder="https://viber.me/user-name" />
                             </Grid>
                         </Grid>
                         <Container padding="20px 0 5px" textAlign="right">
