7838e5d3ce96b01d41cd4347507e598e7162f7f6
murod.rahimjonov <murod.rahimjonov@inoviagroup.se>
2022-08-30 18:15:58 +0500

    added

--- src/components/organism/image.crop.gallery/new.js
+++ src/components/organism/image.crop.gallery/new.js
@@ -1,6 +1,6 @@
 import ImgCrop from 'antd-img-crop';
 import { Upload } from 'antd';
-import React,{useState, useEffect} from "react";
+import React,{useState, useEffect, useMemo} from "react";
 import {CameraOutlined} from "@ant-design/icons"
 import "./style.css"
 import Loader from "react-loader-spinner";
@@ -11,31 +11,30 @@ import { useTranslation } from 'react-i18next';
 const Demo = ({loading, setLoading, list, setCallback, role, urlType}) => {
   const [url, setUrl] = useState({success:'', error:'', loading:'false'})
   const { t } = useTranslation()
-  const onChange = ( newFileList) => {
-    return null
-  };
-  const handleSubmitOk = (e) => {
-    setLoading(true)
-    const form = new FormData()
-    form.append('image',e)
-    postResponse(`/api/${role}s/edit/${urlType}/`, form, setUrl)
-  }
-  useEffect(()=>{
-    if(list.length > 0 && url?.success!==''){
-      setLoading(false)
+  let check = true
+  const onChange = ( e) => {
+    let data = e?.file?.originFileObj
+    if(data && check && !loading){
+      const form = new FormData()
+      form.append('image',data)
+      console.log(data)
+      setLoading(true)
+      postResponse(`/api/${role}s/edit/${urlType}/`, form, setUrl)
     }
-  },[list,url])
+    check = false
+  };
   useEffect(()=>{
     if(url?.success!=='' || url?.error!==''){
       setCallback(prev=>!prev)
     }
     if(url?.success!=='') toast.success(t("MTmenHaqimda.loaded"))
     else if(url?.error!=='') toast.error(t("MTmenHaqimda.notogri"))
+    setLoading(false)
   },[url])
   return(
       <div className="cropper_container ext">
         <div className="icon_wrapper ext">
-            <ImgCrop minZoom={-10} grid aspect={3/2} modalOk="ok" name="image" onModalOk={handleSubmitOk} rotate shape="rect" >
+            {/* <ImgCrop minZoom={-5} grid aspect={1/1} modalOk="ok" name="image" onModalOk={handleSubmitOk} rotate shape="rect" > */}
                 <Upload onChange={onChange} name="image">
                     <div className="icb add_icon gall">
                         {
@@ -43,7 +42,7 @@ const Demo = ({loading, setLoading, list, setCallback, role, urlType}) => {
                         }
                     </div>
                 </Upload>
-            </ImgCrop>
+            {/* </ImgCrop> */}
         </div>
       </div>
     )
